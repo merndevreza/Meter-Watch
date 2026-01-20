adaf9142ec7d2cd31472b3da9d4321c79a159635
merndevreza <merndevreza@gmail.com>
2026-01-20 13:12:57 +0600

    added dark light theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { LoginForm } from "@/app/login/_components/login-form"; 
+import { DarkLightToggle } from "@/components/theme-toggle/DarkLightToggle";
 
 export default function Home() {
   return (
       <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <DarkLightToggle />
         <LoginForm />
       </div>
     </div>
